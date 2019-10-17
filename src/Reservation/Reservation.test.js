27434e0b743e53df000d6b1004a5114a4b9cb5a9
Scott Schipke <swschipke@gmail.com>
2019-10-17 11:04:26 -0600

    Add snapshot test for Reservation

--- src/Reservation/Reservation.test.js
+++ src/Reservation/Reservation.test.js
@@ -3,9 +3,21 @@ import { shallow } from 'enzyme';
 import Reservation from './Reservation'
 
 describe('reservation', () => {
-  let wrapper;
-  const cancelReservation = jest.fn();
+  let wrapper, mockReservation;
+  const mockCancelReservation = jest.fn();
   beforeEach(() =>{
-    
+    mockReservation = {
+      id: 123,
+      name: 'Kimberly',
+      date: '10/25',
+      time: '8:00',
+      number: 6
+    }
+    wrapper =  shallow(
+      <Reservation reservation={mockReservation} cancelReservation={mockCancelReservation}/>
+    )
+  })
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
   })
 })
