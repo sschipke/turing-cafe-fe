41330e4df52bf5c83f437ca6bb77a57730337fbc
Scott Schipke <swschipke@gmail.com>
2019-10-17 09:46:52 -0600

    Set up boiler plate for Form

---
+++ src/Form/Form.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Form.css'
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+
+  render() {
+
+  }
+}
+
+export default Form
