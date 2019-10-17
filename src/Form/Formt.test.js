c5a18cfe8f454765f853a7b93bc79421e903aeba
Scott Schipke <swschipke@gmail.com>
2019-10-17 10:43:25 -0600

    Add tests for changes in state to Form

--- src/Form/Formt.test.js
+++ src/Form/Formt.test.js
@@ -11,4 +11,17 @@ describe('Form', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot()
   })
+  it('should update its state if there is a change', () => {
+    let mockEvent = {target:{id:"name", value:'Greg'}};
+    expect(wrapper.state('name')).toEqual('');
+    wrapper.find('#name').simulate('change', mockEvent);
+    expect(wrapper.state('name')).toEqual('Greg')
+  })
+  it('should reset its state when handleSubmit is called', () => {
+    wrapper.instance().setState({name:'Greg', date:'10/20', time:'7:00'});
+    wrapper.find('button').simulate('click');
+    expect(wrapper.state('name')).toEqual('')
+    expect(wrapper.state('date')).toEqual('')
+    expect(wrapper.state('time')).toEqual('')
+  })
 })
